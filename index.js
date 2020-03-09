a43cd2cc1a3c382396a9cc12698a4f99d26eab1c
Rami Hadad <ramihikmat10@gmail.com>
2020-03-10 08:38:48 +1100

    Partial updating of shape

--- index.js
+++ index.js
@@ -65,26 +65,36 @@ window.addEventListener("load", () => {
     context.lineCap = "round";
 
     // Set event listeners
-
+    // Range events
     range.addEventListener("input", (event) => {
         userPrefs.size = Number.parseInt(event.target.value);
         canvas.getContext("2d").lineWidth = userPrefs.size;
+
+        // Change size of cursor
+        cursor.setAttribute("style", `width: ${1 * userPrefs.size}rem;
+                                      height: ${1* userPrefs.size}rem;`);
     });
 
+    // Change colour of cursor
     colourPicker.addEventListener("input", (event) => {
         userPrefs.colour = event.target.value;
+        console.log(userPrefs.colour);
         canvas.getContext("2d").strokeStyle = userPrefs.colour;
+        console.log(cursor.style.backgroundColor);
         cursor.style.backgroundColor = userPrefs.colour;
+        console.log(cursor.style.backgroundColor);
+
     });
 
     // Show cursor when inside canvas else hide
-    canvas.addEventListener("mouseenter", () => {
-        cursor.setAttribute("style", `visibility: visible;`);
-    });
-    canvas.addEventListener("mouseleave", () => {
-        cursor.setAttribute("style", `visibility: hidden;`);
-    });
+    // canvas.addEventListener("mouseenter", () => {
+    //     cursor.setAttribute("style", `visibility: visible;`);
+    // });
+    // canvas.addEventListener("mouseleave", () => {
+    //     cursor.setAttribute("style", `visibility: hidden;`);
+    // });
 
+    // Movement events
     canvas.addEventListener("mousedown", mouseDown, false);
     canvas.addEventListener("mousemove", mouseMoving, false);
 });
