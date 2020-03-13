abc88da9401f2a259a4699d925c8a65c09c58562
Rami Hadad <ramihikmat10@gmail.com>
2020-03-13 19:49:18 +1100

    Made it an PWA that can be installed

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,8 @@
+const cacheName = "my-pwa-cache";
+
 // FOR PWA INSTALLABILITY
 self.addEventListener("install", (event) => {
-    event.waitUntil(caches.open("my-pwa-cache").then((cache) => {
+    event.waitUntil(caches.open(cacheName).then((cache) => {
         return cache.addAll([
             "/",
             "index.html",
@@ -8,5 +10,16 @@ self.addEventListener("install", (event) => {
             "styles.css",
             "manifest.json"
         ]);
+    }));
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(caches.match(event.request).then((res) => {
+        return res || fetch(event.request).then((res2) => {
+            return caches.open(cacheName).then((cache) => {
+                cache.put(event.request, res2.clone());
+                return res2;
+            });
+        });
     }));
 });
